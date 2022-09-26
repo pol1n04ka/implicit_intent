083dfd6e15caab03a71ae8c2148e44f5db3d96ee
pol1n04ka <314kachu1337@gmail.com>
2022-09-26 18:26:21 +0200

    Implemented implicit intent for image

--- app/src/main/java/com/topolina/implicitintents/MainActivity.kt
+++ app/src/main/java/com/topolina/implicitintents/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.topolina.implicitintents
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import com.topolina.implicitintents.databinding.ActivityMainBinding
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     // Binding for layout
     private lateinit var b: ActivityMainBinding
+
+    val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        b.imageView.setImageURI(it)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         val view = b.root
         setContentView(view)
 
-
+        b.btnChoose.setOnClickListener {
+            getContent.launch("image/*")
+        }
     }
 }
